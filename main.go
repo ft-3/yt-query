6cee5c64e3e2e2fd34320d8bfce092f22f48fcd0
ft3 <f33t@protonmail.com>
2018-05-03 21:51:09 +0200

    Query working. Working on decoding data from JSON

--- main.go
+++ main.go
@@ -6,12 +6,23 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"io/ioutil"
+	"encoding/json"
 )
+
+
+// Data type to store results from api query
+type Results struct {
+	PageToken string  `json:"nextPageToken"`
+	Id map[string]string `json:"id"`
+	Snippet map[string]string `json:"snippet"`
+}
+
 
 func translateToUrl(url string) string {
 	url = strings.TrimSpace(url)
-	newUrl := strings.Replace(url, " ", "+", -1)
-	return "https://hooktube.com/results?search_query="+newUrl
+	newUrl := strings.Replace(url, " ", "%20", -1)
+	return "https://hooktube.com/api?mode=search&q=" + newUrl
 }
 
 func readInput() string {
@@ -20,10 +31,62 @@ func readInput() string {
 	text, _ := reader.ReadString('\n')
 	return text
 }
-		
+
+// Have to figure out a way to pull things out of the items field in the response.
+// Thinking about doing a json.RawMessage type, and making my own decoder for it
+
 
 func main() {
 	text := readInput()
 	str := translateToUrl(text)
-	fmt.Println(str)
+	resp, err := http.Get(str)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Close the body when function ends
+	defer resp.Body.Close()
+
+	// Convert body to []byte
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	// DEBUG
+//	reader := bufio.NewReader(os.Stdin)
+
+	var structured Results
+/*
+	var result interface{}
+	json.Unmarshal(body, &result)
+*/
+	json.Unmarshal(body, &structured)
+
+	fmt.Println(structured.PageToken)
+
+	//m := structured.Items.(map[string]interface{})
+	//m := structured.Items.([]interface{})
+
+	fmt.Println(structured)
+	fmt.Println(structured.Id)
+	fmt.Println(structured.Snippet)
+/*
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string")
+
+		case float64:
+			fmt.Println(k, "is float")
+
+		case []interface{}:
+			fmt.Println(k, "is an array")
+			for i, u := range vv {
+				fmt.Println(i, u)
+				reader.ReadString('\n')
+			}
+		default:
+			fmt.Println(k, "dunno m8")
+		}
+		reader.ReadString('\n')
+	}
+*/
 }
